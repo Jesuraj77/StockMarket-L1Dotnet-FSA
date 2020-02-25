3862cded94ced50bdcf54fe43dbf6a55d02f2a65
Jesuraj77 <isysraju2001@gmail.com>
2020-02-25 13:30:20 +0530

    added

---
+++ Stock Market FSA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Stock_Market_FSA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
